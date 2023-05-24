616987bf4d8f130a7df9d94b273588c6857f1f39
mikechess <mikecook450@gmail.com>
2023-05-24 17:13:09 -0400

    Advanced Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,19 @@
 import './App.css';
 import { Greet } from './components/Greet';
-import { Person } from './components/Person';
-import { PersonList } from './components/PersonList';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
+import { Status } from './components/Status';
 
 function App() {
-  const personName = {
-    first: 'Bruce',
-    last: 'Wayne',
-  }
-
-  const nameList = [
-    {
-      first: "Bruce",
-      last: "Wayne",
-    },
-    {
-      first: "Clark",
-      last: "Kent",
-    },
-    {
-      first: "Princess",
-      last: "Diana",
-    },
-  ]
 
   return (
     <div className="App">
-     <Greet 
-      name="Michael" 
-      messageCount={20} 
-      isLoggedIn={false}
-    />
-    <Person name={personName} />
-    <PersonList names={nameList} />
+     <Status status="error" />
+     <Heading>Placeholder text</Heading>
+     <Oscar>
+      <Heading>Oscar goes to Leonardo Dicapario!</Heading>
+     </Oscar>
+     <Greet name="Michael" isLoggedIn={false} />
     </div>
   );
 }
